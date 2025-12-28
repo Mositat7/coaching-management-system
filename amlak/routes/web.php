9bb020607b2f05da2ea570e71718d65a2d222d41
mositat7 <mositat7@gmail.com>
2025-12-28 13:04:00 +0330

    feat: connect property-add route to PropertyController
    - Update routes/web.php to use PropertyController::create method
    - Add named route 'properties.create' for property addition form
    - Connect /property-add endpoint to controller

--- amlak/routes/web.php
+++ amlak/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\Property\PropertyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard-agent');
 });
-Route::get('/property-add', function () {
-    return view('property-add');
-});
+//بخش املاک
+Route::get('/property-add', [PropertyController::class, 'create'])->name('properties.create');
 
