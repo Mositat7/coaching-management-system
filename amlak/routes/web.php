897e782e56a5f20fde3b9cd9af4e06f3bed7b59b
mositat7 <mositat7@gmail.com>
2025-12-28 13:43:57 +0330

    feat: add property list route
    - Add GET /property-list route
    - Connect to PropertyController::list method
    - Set named route 'properties.list' for property listing page
    - Complete property navigation routes: add, details, list

--- amlak/routes/web.php
+++ amlak/routes/web.php
@@ -9,4 +9,5 @@ Route::get('/', function () {
 //بخش املاک
 Route::get('/property-add', [PropertyController::class, 'add'])->name('properties.add');
 Route::get('/property-details', [PropertyController::class, 'details'])->name('properties.details');
+Route::get('/property-list', [PropertyController::class, 'list'])->name('properties.list');
 
