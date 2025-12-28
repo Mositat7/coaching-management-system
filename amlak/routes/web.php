889434865e391359197b4c26c7df63a693e8b690
mositat7 <mositat7@gmail.com>
2025-12-29 01:29:16 +0330

    routes: Add coach profile route
    - Add GET /coach-add route for coach profile page
    - Map to CoachController@add method
    - Name route 'coach.add'
    - Prepare for coach profile management

--- amlak/routes/web.php
+++ amlak/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Coach\CoachController;
 use App\Http\Controllers\Members\MemberController;
 use App\Http\Controllers\Property\PropertyController;
 use Illuminate\Support\Facades\Route;
@@ -12,8 +13,10 @@ Route::get('/property-add', [PropertyController::class, 'add'])->name('propertie
 Route::get('/property-details', [PropertyController::class, 'details'])->name('properties.details');
 Route::get('/property-list', [PropertyController::class, 'list'])->name('properties.list');
 Route::get('/property-grid', [PropertyController::class, 'grid'])->name('properties.grid');
-//بحش مشتری ها
+//بحش عضو ها
 Route::get('/members-add', [MemberController::class, 'add'])->name('members.add');
 Route::get('/members-details', [MemberController::class, 'details'])->name('members.details');
 Route::get('/members-list', [MemberController::class, 'list'])->name('members.list');
 Route::get('/members-grid', [MemberController::class, 'grid'])->name('members.grid');
+// بخش مربی
+Route::get('/Coach-add', [CoachController::class, 'add'])->name('Coach.add');
