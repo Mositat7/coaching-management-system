07bc0aa72a33d495072b4315807796f17c43a313
mositat7 <mositat7@gmail.com>
2025-12-28 12:40:24 +0330

    feat(dashboard): remove unused dashboard-agent.html

--- amlak/routes/web.php
+++ amlak/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('dashboard-agent');
+});
+Route::get('/property-add', function () {
     return view('property-add');
 });
+
