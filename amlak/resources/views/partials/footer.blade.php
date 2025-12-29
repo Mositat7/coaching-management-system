f7ee545b59ec79c9431cd06bea87ed5096ae6a37
mositat7 <mositat7@gmail.com>
2025-12-29 13:18:06 +0330

    style: update footer design
    - Customize footer layout
    - Add personal branding elements
    - Improve responsive behavior
    - Update contact information
    - Enhance visual appearance

--- amlak/resources/views/partials/footer.blade.php
+++ amlak/resources/views/partials/footer.blade.php
@@ -4,11 +4,11 @@
             <script>
                 document.write(new Date().getFullYear())
             </script>
-            &copy; لاهومز. طراحی شده توسط
+            &copy;توسعه یافته توسط
             <iconify-icon class="fs-18 align-middle text-danger" icon="solar:hearts-bold-duotone">
             </iconify-icon>
             <a class="fw-bold footer-text" href="#" target="_blank">
-                iarsalan
+                Mostafa Tatar
             </a>
         </div>
     </div>
